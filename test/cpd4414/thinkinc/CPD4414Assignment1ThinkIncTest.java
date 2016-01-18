dae1924456114d6e036f66871ab2e49cc2f3df86
Brad Ryan <brad.m.ryan@gmail.com>
2016-01-18 03:25:11 -0500

    Bonus Challenge.

--- test/cpd4414/thinkinc/CPD4414Assignment1ThinkIncTest.java
+++ test/cpd4414/thinkinc/CPD4414Assignment1ThinkIncTest.java
@@ -5,6 +5,7 @@
  */
 package cpd4414.thinkinc;
 
+import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -72,5 +73,34 @@ public class CPD4414Assignment1ThinkIncTest {
         String result = CPD4414Assignment1ThinkInc.stringPower(str, integer);
         assertEquals(expResult, result);
     }
+    
+    @Test
+    public void testRandomStringAndRandomIntegerShouldReturnRandomStringTimesRandomInteger() {
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int integer;
+        String str;
+        String expResult;
         
+        StringBuilder sbExpResult = new StringBuilder();
+        StringBuilder sbStr = new StringBuilder();
+        Random rand = new Random();
+        
+        //Build random 3 letter string
+        for (int i = 0; i < 3; i++){
+            char c = alphabet.charAt(rand.nextInt(alphabet.length()));
+            sbStr.append(c);
+        }
+        
+        integer = rand.nextInt(10) + 1; //Generate random number between 1 and 10
+        str = sbStr.toString();
+        
+        //Build expected result by multiplying string by random integer
+        for (int i = 0; i < integer; i++) {
+            sbExpResult.append(str);
+        }
+        
+        expResult = sbExpResult.toString();
+        String result = CPD4414Assignment1ThinkInc.stringPower(str, integer);
+        assertEquals(expResult, result);
+    }
 }
